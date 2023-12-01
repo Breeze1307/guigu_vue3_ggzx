2257b3d2d9fa8f99916d587fe41dd55336e73ba3
breeze1307 <breeze011307@gmail.com>
2023-12-01 17:40:41 +0800

    接口统一管理

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -2,7 +2,18 @@
  * @Description:
  * @Author: breeze1307
  * @Date: 2023-11-30 09:26:47
- * @LastEditTime: 2023-11-30 10:19:51
+ * @LastEditTime: 2023-12-01 10:02:23
  * @LastEditors: breeze1307
  */
 import request from '@/utils/request'
+import type {
+  loginForm,
+  loginResponseData,
+  userResponseData,
+} from '@/api/user/type'
+// 登录接口
+export const login = (data: loginForm) =>
+  request.post<any, loginResponseData>('/user/login', data)
+// 获取用户信息
+export const getUserInfo = () =>
+  request.get<any, userResponseData>('/user/info')
---
+++ src/api/user/type.ts
@@ -0,0 +1,38 @@
+/*
+ * @Description:
+ * @Author: breeze1307
+ * @Date: 2023-11-30 09:27:09
+ * @LastEditTime: 2023-12-01 10:54:52
+ * @LastEditors: breeze1307
+ */
+// 登录接口需要携带参数ts类型
+export interface loginForm {
+  username: string
+  password: string
+}
+interface dataType {
+  token: string
+}
+// 登录接口返回数据类型
+export interface loginResponseData {
+  code: number
+  data: dataType
+}
+interface user {
+  checkUser: {
+    userId: number
+    avatar: string
+    username: string
+    password: string
+    desc: string
+    roles: string[]
+    buttons: string[]
+    routes: string[]
+    token: string
+  }
+}
+// 定义服务器返回用户信息相关的数据类型
+export interface userResponseData {
+  code: number
+  data: user
+}

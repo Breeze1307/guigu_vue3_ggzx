76cfa82970f4e00fd3afe5fc91bb41f0878e72e6
breeze1307 <breeze011307@gmail.com>
2023-12-15 18:15:04 +0800

    测试cors跨域

--- .d.ts
+++ .d.ts
@@ -1,3 +1,11 @@
+/*
+ * @Description: 
+ * @Author: breeze1307
+ * @Date: 2023-11-28 15:13:05
+ * @LastEditTime: 2023-12-15 14:52:22
+ * @LastEditors: breeze1307
+ */
 declare module 'mockjs'
 declare module 'express'
+declare module 'cors'
 declare module 'nprogress'
--- MockServer.ts
+++ MockServer.ts
@@ -2,23 +2,24 @@
  * @Description:
  * @Author: breeze1307
  * @Date: 2023-11-28 19:48:50
- * @LastEditTime: 2023-11-30 14:04:34
+ * @LastEditTime: 2023-12-15 18:07:37
  * @LastEditors: breeze1307
  */
 import express from 'express'
+// import cors from 'cors'
 let app = express()
 // 使用中间件解析json请求体
 app.use(express.json())
+// app.use(cors())
+
 import router from './MockServer/api.ts'
 import userRouter from './MockServer/user.ts'
-
-// app.use('*', function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*")
-//     res.header("Access-Control-Allow-Methods", "PUT,GET,POST,DELETE,OPTIONS")
-//     res.header("Access-Control-Allow-Headers", "X-Requested-With")
-//     res.header("Access-Control-Allow-Headers","Content-Type")
-//     next()
-// })
+app.use('*',function(_req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT,GET,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,token');
+  next()
+})
 app.use(router)
 
 app.use(userRouter)

c786fc11f7eaf650a36c4bb52b6ceb7724fe90ae
breeze1307 <breeze011307@gmail.com>
2023-12-21 16:39:41 +0800

    品牌表单验证

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: breeze1307
  * @Date: 2023-12-12 15:53:59
- * @LastEditTime: 2023-12-20 20:11:47
+ * @LastEditTime: 2023-12-21 16:35:25
  * @LastEditors: breeze1307
 -->
 <template>
@@ -19,7 +19,7 @@
           <img :src="row.logoUrl" alt="" style="width: 100px; height: 100px" />
         </template>
       </el-table-column>
-      <el-table-column label="品牌操作">
+      <el-table-column label="品牌操作" style="white-space: nowrap;">
         <template #="{ row }">
           <el-button
             sizes="small"
@@ -51,14 +51,21 @@
     />
     <!-- 添加/修改品牌对话框 -->
     <el-dialog v-model="dialogFormVisible" title="添加品牌" width="30%">
-      <el-form label-width="100px" class="paramsForm">
-        <el-form-item label="品牌名称">
+      <el-form
+        label-width="100px"
+        class="paramsForm"
+        :rules="rules"
+        :model="trademarkParams"
+        ref="ruleFormRef"
+      >
+        <el-form-item label="品牌名称" prop="tmName">
           <el-input v-model="trademarkParams.tmName" autocomplete="off" />
         </el-form-item>
-        <el-form-item label="品牌LOGO">
+        <el-form-item label="品牌LOGO" prop="logoUrl">
           <template #>
             <el-upload
               drag
+              :show-file-list="false"
               action="/api/admin/product/fileUpload"
               :on-success="handleImgSuccess"
               :before-upload="beforeImgUpload"
@@ -89,14 +96,14 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, reactive, onMounted } from 'vue'
+import { ref, reactive, onMounted, nextTick } from 'vue'
 import {
   reqHasTrademark,
   reqAddOrUpdateTrademark,
   reqDeleteTrademark,
 } from '@/api/product/trademark'
 import { ElMessage } from 'element-plus'
-import type { UploadProps } from 'element-plus'
+import type { UploadProps, FormRules } from 'element-plus'
 import type {
   TrademarkResponseData,
   Records,
@@ -117,6 +124,8 @@ let trademarkParams = reactive<Trademark>({
   tmName: '',
   logoUrl: '',
 })
+// 获取表单
+let ruleFormRef = ref()
 // 获取已有品牌信息
 const getHasTrademark = async (page = 1) => {
   pageNo.value = page
@@ -133,6 +142,26 @@ const getHasTrademark = async (page = 1) => {
 const changeLimit = () => {
   getHasTrademark()
 }
+// 表单验证规则
+const validateTmName = (_rule: any, value: any, callback: any) => {
+  if (value.trim().length < 2) {
+    callback(new Error('品牌名称不能小于两个字符'))
+  } else {
+    callback()
+  }
+}
+const validateLogoUrl = (_rule: any, value: any, callback: any) => {
+  if (value) {
+    callback()
+  } else {
+    callback(new Error('请上传品牌图标'))
+  }
+}
+// 添加/修改商品表单验证
+const rules = reactive<FormRules<typeof trademarkParams>>({
+  tmName: [{ required: true, validator: validateTmName, trigger: 'blur' }],
+  logoUrl: [{ required: true, validator: validateLogoUrl }],
+})
 // 上传图片成功之前，检查类型，大小是否符合
 const beforeImgUpload: UploadProps['beforeUpload'] = (rawFile) => {
   if (
@@ -153,16 +182,24 @@ const beforeImgUpload: UploadProps['beforeUpload'] = (rawFile) => {
 }
 // 上传品牌图片成功
 const handleImgSuccess: UploadProps['onSuccess'] = (response) => {
-  console.log(response)
-
   trademarkParams.logoUrl = response.data
+  // 图片上传成功，清除验证信息
+  ruleFormRef.value.clearValidate('logoUrl')
 }
 // 添加/修改品牌请求
 const addOrUpdateTrademark = async () => {
+  // 表单内容全部验证,全部验证成功后才允许发送请求
+  await ruleFormRef.value.validate()
   let result: any = await reqAddOrUpdateTrademark(trademarkParams)
   if (result.code == 200) {
     dialogFormVisible.value = false
-    getHasTrademark()
+    if (trademarkParams.id) {
+      // 修改品牌获取当前页数据
+      getHasTrademark(pageNo.value)
+    } else {
+      // 添加品牌获取数据,向上取整，获取最后一页数据
+      getHasTrademark(Math.ceil((total.value + 1) / limit.value))
+    }
     ElMessage.success(trademarkParams.id ? '修改品牌成功' : '添加品牌成功')
   } else {
     ElMessage.error(trademarkParams.id ? '修改品牌失败' : '添加品牌失败')
@@ -174,11 +211,20 @@ const addTrademark = () => {
   trademarkParams.id = 0
   trademarkParams.tmName = ''
   trademarkParams.logoUrl = ''
+  // 首次点击会报错，使用？语法糖或者nextTick
+  ruleFormRef.value?.clearValidate('tmName')
+  ruleFormRef.value?.clearValidate('logoUrl')
 }
 // 点击修改按钮
 const updateTrademark = (row: Trademark) => {
   dialogFormVisible.value = true
   Object.assign(trademarkParams, row)
+  // ruleFormRef.value?.clearValidate('tmName')
+  // ruleFormRef.value?.clearValidate('logoUrl')
+  nextTick(() => {
+    ruleFormRef.value.clearValidate('tmName')
+    ruleFormRef.value.clearValidate('logoUrl')
+  })
 }
 // 删除品牌
 const deleteTrademark = async (id: number) => {

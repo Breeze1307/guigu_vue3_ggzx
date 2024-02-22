972f045d473c5e80fb6bdd24f71195584ebd7beb
breeze1307 <breeze011307@gmail.com>
2024-02-22 17:39:25 +0800

    用户管理静态搭建

--- src/views/acl/user/index.vue
+++ src/views/acl/user/index.vue
@@ -2,13 +2,65 @@
  * @Description: 
  * @Author: breeze1307
  * @Date: 2023-12-12 15:30:37
- * @LastEditTime: 2023-12-12 15:31:23
+ * @LastEditTime: 2024-02-22 17:35:55
  * @LastEditors: breeze1307
 -->
 <template>
-  <div>用户管理</div>
+  <el-card>
+    <el-form :inline="true">
+      <el-form-item label="用户名">
+        <el-input
+          placeholder="请输入搜索用户名"
+          v-model="searchName"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="search">搜索</el-button>
+        <el-button type="primary" @click="reset">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
+  <el-card>
+    <el-button type="primary" @click="addUser">添加用户</el-button>
+    <el-button type="primary" @click="deleteUser">删除用户</el-button>
+    <el-table :data="userInfo">
+      <el-table-column type="selection" width="60" />
+      <el-table-column type="index" label="#" width="60" />
+      <el-table-column prop="address" label="ID" />
+      <el-table-column prop="address" label="用户名字" />
+      <el-table-column prop="address" label="用户名称" />
+      <el-table-column prop="address" label="用户角色" />
+      <el-table-column prop="address" label="创建时间" />
+      <el-table-column prop="address" label="更新时间" />
+      <el-table-column label="操作">
+        <template #="{ row }">
+          <el-button type="primary" icon="User" size="small">分配角色</el-button>
+          <el-button type="primary" icon="Edit" size="small">编辑</el-button>
+          <el-button type="primary" icon="Delete" size="small">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+// 用户数据列表
+let userInfo = ref<any>({})
+// 搜索关键字
+let searchName = ref<string>('')
+// 搜索
+const search = () => {}
+// 重置
+const reset = () => { }
+// 添加用户
+const addUser = () => {
+  
+}
+// 删除用户 
+const deleteUser = () => {
+  
+}
+</script>
 
 <style lang="scss" scoped></style>

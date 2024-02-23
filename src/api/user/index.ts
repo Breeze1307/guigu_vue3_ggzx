042c3f195ef69bed6f34d100283f89df1eebd742
breeze1307 <breeze011307@gmail.com>
2024-02-23 17:51:55 +0800

    格式修复

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -12,7 +12,7 @@ import type {
   userResponseData,
   UserInfoList,
   UserInfo,
-  AllRoleInfo
+  AllRoleInfo,
 } from '@/api/user/type'
 // 登录接口
 export const login = (data: loginForm) =>
@@ -23,16 +23,18 @@ export const getUserInfo = () =>
 // 退出登录
 export const logOut = () => request.post<any, any>('/admin/acl/index/logout')
 // 用户列表获取
-export const reqUserList = (page:number,limit:number,username:string) =>
-  request.get<any, UserInfoList>(`/admin/acl/user/${page}/${limit}`, { params: { username } })
+export const reqUserList = (page: number, limit: number, username: string) =>
+  request.get<any, UserInfoList>(`/admin/acl/user/${page}/${limit}`, {
+    params: { username },
+  })
 // 用户新增或修改
-export const reqUserAddOrUpdate = (data:UserInfo) => {
+export const reqUserAddOrUpdate = (data: UserInfo) => {
   if (data.id) {
-    return request.put<any,any>('/admin/acl/user/update',data)
+    return request.put<any, any>('/admin/acl/user/update', data)
   } else {
-    return request.post<any,any>('/admin/acl/user/save',data)
+    return request.post<any, any>('/admin/acl/user/save', data)
   }
 }
 // 用户角色信息
-export const reqUserRole = (userId:number) => 
-  request.get<any,AllRoleInfo>(`/admin/acl/user/toAssign/${userId}`)
+export const reqUserRole = (userId: number) =>
+  request.get<any, AllRoleInfo>(`/admin/acl/user/toAssign/${userId}`)
--- src/views/acl/user/index.vue
+++ src/views/acl/user/index.vue
@@ -15,15 +15,21 @@
         ></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="search" :disabled="searchName?false:true">搜索</el-button>
+        <el-button
+          type="primary"
+          @click="search"
+          :disabled="searchName ? false : true"
+        >
+          搜索
+        </el-button>
         <el-button type="primary" @click="reset">重置</el-button>
       </el-form-item>
     </el-form>
   </el-card>
-  <el-card style="margin-top: 10px;">
+  <el-card style="margin-top: 10px">
     <el-button type="primary" @click="addUser">添加用户</el-button>
     <el-button type="primary" @click="deleteUser">删除用户</el-button>
-    <el-table :data="userInfo" border style="margin: 10px 0;">
+    <el-table :data="userInfo" border style="margin: 10px 0">
       <el-table-column type="selection" width="60" />
       <el-table-column type="index" label="#" width="60" />
       <el-table-column prop="id" label="ID" />
@@ -34,10 +40,22 @@
       <el-table-column prop="updateTime" label="更新时间" />
       <el-table-column label="操作" width="300px" align="center">
         <template #="{ row }">
-          <el-button type="primary" icon="User" size="small" @click="assignRole">
+          <el-button
+            type="primary"
+            icon="User"
+            size="small"
+            @click="assignRole"
+          >
             分配角色
           </el-button>
-          <el-button type="primary" icon="Edit" size="small" @click="editUser(row)">编辑</el-button>
+          <el-button
+            type="primary"
+            icon="Edit"
+            size="small"
+            @click="editUser(row)"
+          >
+            编辑
+          </el-button>
           <el-button type="primary" icon="Delete" size="small">删除</el-button>
         </template>
       </el-table-column>
@@ -57,17 +75,27 @@
   <!-- 添加用户 -->
   <el-drawer
     v-model="userDrawer"
-    :title="userParams.id?'修改用户':'添加用户'"
+    :title="userParams.id ? '修改用户' : '添加用户'"
   >
     <template #default>
       <el-form :model="userParams" :rules="userRules" ref="userRef">
         <el-form-item label="用户姓名" prop="username">
-          <el-input placeholder="请输入姓名" v-model="userParams.username"></el-input>
+          <el-input
+            placeholder="请输入姓名"
+            v-model="userParams.username"
+          ></el-input>
         </el-form-item>
         <el-form-item label="用户昵称" prop="name">
-          <el-input placeholder="请输入昵称" v-model="userParams.name"></el-input>
-        </el-form-item><el-form-item label="用户密码" prop="password" v-show="!userParams.id">
-          <el-input placeholder="请输入密码" v-model="userParams.password"></el-input>
+          <el-input
+            placeholder="请输入昵称"
+            v-model="userParams.name"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="用户密码" prop="password" v-show="!userParams.id">
+          <el-input
+            placeholder="请输入密码"
+            v-model="userParams.password"
+          ></el-input>
         </el-form-item>
       </el-form>
     </template>
@@ -77,16 +105,14 @@
     </template>
   </el-drawer>
   <!-- 分配角色 -->
-  <el-drawer title="分配角色（职位）" v-model="roleDrawer">
-
-  </el-drawer>
+  <el-drawer title="分配角色（职位）" v-model="roleDrawer"></el-drawer>
 </template>
 
 <script lang="ts" setup>
-import { ref, reactive,onMounted ,nextTick} from 'vue'
-import { reqUserList,reqUserAddOrUpdate,reqUserRole } from '@/api/user';
-import type { UserInfoList,UserInfo,AllRoleInfo } from '@/api/user/type';
-import { ElMessage } from 'element-plus';
+import { ref, reactive, onMounted, nextTick } from 'vue'
+import { reqUserList, reqUserAddOrUpdate, reqUserRole } from '@/api/user'
+import type { UserInfoList, UserInfo, AllRoleInfo } from '@/api/user/type'
+import { ElMessage } from 'element-plus'
 // 用户数据列表
 let userInfo = ref<any>([])
 // 当前页
@@ -94,43 +120,47 @@ let pageNo = ref<number>(1)
 // 限制条数
 let limit = ref<number>(5)
 // 总数
-let total=ref<number>(0)
+let total = ref<number>(0)
 // 搜索关键字
 let searchName = ref<string>('')
 // 添加用户面板控制
 let userDrawer = ref<boolean>(false)
 // 用户信息收集
 let userParams = reactive<UserInfo>({
-  username:'',
+  username: '',
   name: '',
-  password:''
+  password: '',
 })
 // form组件实例
 let userRef = ref<any>()
 // 分配职位抽屉控制
-let roleDrawer=ref<boolean>(false)
+let roleDrawer = ref<boolean>(false)
 // 用户验证信息
 const userRules = {
-  username:[
-    {required: true, min: 2, message: '用户名字至少两位', trigger: 'blur'}
+  username: [
+    { required: true, min: 2, message: '用户名字至少两位', trigger: 'blur' },
   ],
   name: [
-    {required: true, min: 5, message: '用户昵称至少五位', trigger: 'blur'}
+    { required: true, min: 5, message: '用户昵称至少五位', trigger: 'blur' },
   ],
   password: [
-    {required: true, min: 6, message: '用户密码至少六位', trigger: 'blur'}
-  ]
+    { required: true, min: 6, message: '用户密码至少六位', trigger: 'blur' },
+  ],
 }
 onMounted(() => {
   getUserList()
 })
 // 获取用户列表
 const getUserList = async (page = 1) => {
-  pageNo.value=page
-  let result: UserInfoList = await reqUserList(pageNo.value, limit.value,searchName.value)
+  pageNo.value = page
+  let result: UserInfoList = await reqUserList(
+    pageNo.value,
+    limit.value,
+    searchName.value,
+  )
   if (result.code == 200) {
     userInfo.value = result.data.records
-    total.value=result.data.total
+    total.value = result.data.total
   }
 }
 // 每页限制条数触发
@@ -143,7 +173,7 @@ const search = () => {
 }
 // 重置
 const reset = () => {
-  searchName.value=''
+  searchName.value = ''
   getUserList()
 }
 // 添加用户
@@ -152,7 +182,7 @@ const addUser = () => {
   clearUser()
 }
 // 编辑用户信息
-const editUser = (row:UserInfo) => {
+const editUser = (row: UserInfo) => {
   Object.assign(userParams, row)
   userDrawer.value = true
   nextTick(() => {
@@ -161,7 +191,7 @@ const editUser = (row:UserInfo) => {
   })
 }
 // 提交用户信息
-const save =async () => {
+const save = async () => {
   await userRef.value.validate()
   let result: any = await reqUserAddOrUpdate(userParams)
   if (result.code == 200) {
@@ -169,7 +199,7 @@ const save =async () => {
     userDrawer.value = false
     getUserList()
     //当用户修改了自己的信息，之前的登录信息失效，浏览器自动刷新一次，进入登录页面
-    window.location.reload();
+    window.location.reload()
   } else {
     ElMessage.success(userParams.id ? '修改失败' : '添加失败')
   }
@@ -185,7 +215,7 @@ const clearUser = () => {
     id: 0,
     username: '',
     name: '',
-    password:''
+    password: '',
   })
   nextTick(() => {
     userRef.value.clearValidate('username')
@@ -194,20 +224,18 @@ const clearUser = () => {
   })
 }
 // 分配角色
-const assignRole = () => {
-  
-}
+const assignRole = () => {}
 // 删除用户
-const deleteUser = () => { }
+const deleteUser = () => {}
 </script>
 
 <style lang="scss" scoped>
-.form{
+.form {
   display: flex;
   justify-content: space-between;
   align-items: center;
-   .el-form-item{
-    margin-bottom:none
+  .el-form-item {
+    margin-bottom: none;
   }
 }
 </style>
